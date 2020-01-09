9d98be3b778f54baa2adf258bb6d431df1709cc7
Marc Jamot <marcjamot@gmail.com>
2020-01-09 15:13:14 +0100

    Update data content in sql

--- sql/V1__Add_posts_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE posts
-(
-    id        UUID PRIMARY KEY,
-    user_name TEXT NOT NULL,
-    content   TEXT NOT NULL
-);
--- sql/V2__Add_example_post.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO posts (id, user_name, content)
-VALUES ('2119477a-d12e-4533-b8ff-45e6a8ce4f75', 'marc', 'This is an example post.');
