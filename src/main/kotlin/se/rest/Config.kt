1168561d206b3bb0b80b8d8b48536643d2d2fedb
Marc Jamot <marcjamot@gmail.com>
2020-04-28 20:37:18 +0200

    Add PG port

--- src/main/kotlin/se/rest/Config.kt
+++ src/main/kotlin/se/rest/Config.kt
@@ -5,6 +5,7 @@ import io.quarkus.arc.config.ConfigProperties
 @ConfigProperties(prefix = "postgres")
 interface PostgresConfig {
     fun getHost(): String
+    fun getPort(): Int
     fun getUser(): String
     fun getPassword(): String
     fun getDatabase(): String
